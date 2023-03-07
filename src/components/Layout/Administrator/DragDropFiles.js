6516e4e704112418f0c03d1ca454139dd437bc5e
Gregori <greg.gego@gmail.com>
2023-03-07 23:59:08 +0100

    logic for multiple post photos to db

--- src/components/Layout/Administrator/DragDropFiles.js
+++ src/components/Layout/Administrator/DragDropFiles.js
@@ -6,26 +6,36 @@ import axios from "axios";
 import classes from "./DragDropFiles.module.css";
 
 const DragDropFiles = () => {
-  const [selectedFiles, setSelectedFiles] = useState([]);
-  const [imagePreviews, setImagePreviews] = useState([]);
-  
+  // const [selectedFiles, setSelectedFiles] = useState([]); old logic only for a single photo
+
+  const [imagePreviews, setImagePreviews] = useState([]); //state to keep img preview
+
+  //multiple photos
+  const [arrayOfPhotos, setArrayOfPhotos] = useState([]);
 
   const selectFiles = (event) => {
-    let images = [];
+    let images = []; //Image preview array only to show the photos that are selected to be uploaded
+    let photos = []; //array of photos for db POST
 
     for (let i = 0; i < event.target.files.length; i++) {
-      images.push(URL.createObjectURL(event.target.files[i]));
+      images.push(URL.createObjectURL(event.target.files[i])); // The use of URL.createObjectURL() is to get the image preview URL and put it into imagePreviews array. This method produces a DOMString including a URL describing the object provided in the parameter. The URL life is tied to the document in the window on which it was created.
+      photos.push(event.target.files[i]);
     }
 
-    setSelectedFiles(event.target.files[0]);
+    // setSelectedFiles(event.target.files[0]);
     setImagePreviews(images);
+    setArrayOfPhotos(photos);
   };
 
-  
-  const sendPhotos = async () => {
+  // A function to call send photos to DB depending on its Length
+  const callUploadMultiple = () => {
+    arrayOfPhotos.map((file, i) => sendPhotos(file));
+  }
 
-    const formData = new FormData();
-    formData.append("files", selectedFiles);
+  const sendPhotos = async (file) => {
+
+    let formData = new FormData();
+    formData.append("files", file);
 
     try {
       const res = await axios.post(
@@ -47,14 +57,14 @@ const DragDropFiles = () => {
           accept="image/*"
           onChange={selectFiles}
         />
-        <button onClick={sendPhotos}>Upload</button>
+        <button onClick={callUploadMultiple}>Upload</button>
       </div>
       {imagePreviews && (
         <div className={classes.imagePreview}>
           {imagePreviews.map((img, i) => {
             return (
               <div key={i} className={classes.imagePreviewWrapper}>
-                <img src={img} alt={"image-" + i}  />
+                <img src={img} alt={"image-" + i} />
               </div>
             );
           })}
